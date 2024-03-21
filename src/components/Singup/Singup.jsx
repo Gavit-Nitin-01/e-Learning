d24a31ca0f11c81163d506f0cc35bac39f1b0718
Gavit-Nitin-01 <gavitnitinratanbhai@gmail.com>
2024-03-21 16:18:29 +0530

    singup and login with database

--- src/components/Singup/Singup.jsx
+++ src/components/Singup/Singup.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './Sing.css'
+import { useNavigate } from 'react-router-dom';
 export default function Singup() {
 
     const [show, setShow] = useState(false);
@@ -16,7 +17,7 @@ export default function Singup() {
         const loginBtn = document.querySelector("label.login");
         const signupBtn = document.querySelector("label.signup");
         const signupLink = document.querySelector("form .signup-link a");
-        
+
         signupBtn.onclick = (() => {
             loginForm.style.marginLeft = "-50%";
             loginText.style.marginLeft = "-50%";
@@ -31,11 +32,53 @@ export default function Singup() {
         });
     }
 
+    //student registration 
+    const [credentials, setCredentials] = useState({ name: "",phone:"", email: "", password: "", cpassword: "" })
+    let navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { name,phone, email, password } = credentials;
+        const response = await fetch("http://localhost:4000/api/student/createstd", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name,phone, email, password })
+        });
+        const json = await response.json();
+        console.log(json)
+        localStorage.setItem('token', json.authtoken)
+        alert("Registration Successfuly")
+        navigate('/');
+
+    }
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+
+    //studetn login 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:4000/api/student/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json();
+        console.log(json)
+        if (json.success) {
+            localStorage.setItem('token', json.authtoken)
+            alert('Login Successfuly');
+            navigate('/course');
+        } else {
+            alert('invalid details');
+        }
+    }
 
     return (
         <>
             <Button className='btn btn-primary mx-2' onClick={handleShow}>
-               SignUp
+                SignUp
             </Button>
 
             <Modal show={show} onHide={handleClose}>
@@ -57,12 +100,12 @@ export default function Singup() {
                                 <div className="slider-tab"></div>
                             </div>
                             <div className="form-inner">
-                                <form action="#" onClick={handleClick} className="login">
+                                <form action="#" onClick={handleClick} onSubmit={handleLogin} className="login">
                                     <div className="field">
-                                        <input type="text" placeholder="Email Address" required />
+                                        <input type="email"  value={credentials.email} onChange={onChange} name='email' placeholder="Email Address" required />
                                     </div>
                                     <div className="field">
-                                        <input type="password" placeholder="Password" required />
+                                        <input type="password"  value={credentials.email} onChange={onChange} name='password' placeholder="Password" required />
                                     </div>
                                     <div className="pass-link"><a href="#">Forgot password?</a></div>
                                     <div className="field btn">
@@ -71,23 +114,23 @@ export default function Singup() {
                                     </div>
                                     <div className="signup-link">Not a member? <a href="">Signup now</a></div>
                                 </form>
-                                <form action="#" onClick={handleClick} className="signup">
+                                <form onClick={handleClick} onSubmit={handleSubmit} className="signup">
                                     <div className="field">
-                                        <input type="text" placeholder="Enter Name" required />
+                                        <input type="text" name='name' onChange={onChange} placeholder="Enter Name" minLength={3} required />
                                     </div>
                                     <div className="field">
-                                        <input type="email" placeholder="Email Address" required />
+                                        <input type="numbers" name='phone' onChange={onChange} placeholder="Phone Number" minLength={10} required />
                                     </div>
                                     <div className="field">
-                                        <input type="numbers" placeholder="Phone Number" required />
+                                        <input type="email" name='email' onChange={onChange} placeholder="Email Address" required />
                                     </div>
                                     <div className="field">
-                                        <input type="password" placeholder="Password" required />
+                                        <input type="password" name='password' onChange={onChange} placeholder="Password" minLength={6} required />
                                     </div>
                                     <div className="field">
-                                        <input type="password" placeholder="Confirm password" required />
+                                        <input type="password" name='cpass' onChange={onChange} placeholder="Confirm password" minLength={6} required />
                                     </div>
-                                    <div className="field btn">
+                                    <div className="field btn"> 
                                         <div className="btn-layer"></div>
                                         <input type="submit" value="Signup" />
                                     </div>
